0f45cba829761413f3f338d0036598d2b204a078
Amjid khan <96972899+HelloAmjidkhan@users.noreply.github.com>
2022-02-15 18:51:43 +0500

    changing in main manu
    changing in main manu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,50 @@
 import React from 'react';
 import {Navbar , Container , Nav} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
+import {FormControl} from 'react-bootstrap';
+import {Form} from 'react-bootstrap';
+import {Offcanvas} from 'react-bootstrap';
 import './Styles.css';
 
 const Header = () => {
   return <div>
-    <Navbar className='navbar'>
-    <Container>
+   
+<Navbar className='navbar' expand={false}>
+  <Container fluid>
     <img src="./images/logo.png" alt="" className='logo-img'/>
-    <Nav className='float-right'>
-      <Nav.Link href="#home" className='text-white' id="nav-text">Home</Nav.Link>
-      <Nav.Link href="#features" className='text-white' id="nav-text">Services</Nav.Link>
-      <Nav.Link href="#pricing" className='text-white' id="nav-text">About</Nav.Link>
-      <Nav.Link href="#pricing" className='text-white' id="nav-text">Skills</Nav.Link>
-      <Nav.Link href="#pricing" className='text-white' id="nav-text">Protfolio</Nav.Link>
-      <Nav.Link href="#pricing" className='text-white' id="nav-text">Contact</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
+    <Navbar.Toggle aria-controls="offcanvasNavbar" id="toggle" />
+    <Navbar.Offcanvas
+      id="offcanvasNavbar"
+      aria-labelledby="offcanvasNavbarLabel"
+      placement="end"
+    >
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Nav className="justify-content-end flex-grow-1 pe-3">
+          <Nav.Link href="#action1">Home</Nav.Link>
+          <Nav.Link href="#action2">My Services</Nav.Link>
+          <Nav.Link href="#action2">About Us</Nav.Link>
+          <Nav.Link href="#action2">My Skills</Nav.Link>
+          <Nav.Link href="#action2">Portfolio</Nav.Link>
+          <Nav.Link href="#action2">Contact Us</Nav.Link>
+        </Nav>
+        <Form className="d-flex">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Offcanvas.Body>
+    </Navbar.Offcanvas>
+  </Container>
+</Navbar>
   </div>;
 };
 
 export default Header;
+
